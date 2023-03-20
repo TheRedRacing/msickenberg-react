355e89a1808d80a2b1408852dfa00a9e579a2813
Maxime Sickenberg <maxylou55@yahoo.com>
2023-03-20 13:50:08 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["fr"],
+    defaultLocale: "fr",
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+export default config;
